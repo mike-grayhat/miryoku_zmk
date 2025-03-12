6536219cfb774a3862a6994e6c962bb0c9f2f769
Mikhail Oskin <mikhail.osckin@gmail.com>
2025-03-12 07:48:22 +0400

    42 modification

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -239,7 +239,7 @@ U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp 
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
 &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,             &kp J,             &kp K,             &kp L,             &kp SQT,           \
 &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
-U_NP,              U_NP,              &kp LCTRL,         &kp LALT,          &kp LSHFT,         &kp ESC,           &kp RET,           &u_to_U_BASE,      U_NP,              U_NP
+U_NP,              U_NP,              &kp LCTRL,         &kp LALT,          &kp LSHFT,         &kp RET,           &kp ESC,           &u_to_U_BASE,      U_NP,              U_NP
 
 #define MIRYOKU_ALTERNATIVES_TAP_QWERTZ \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Z,             &kp U,             &kp I,             &kp O,             &kp P,             \
